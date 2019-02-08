11fc2555d5a10eee6a0a0573ee0b37d33d89bab4
Mr. Bartucz <jobartucz@isd535.org>
2019-02-08 05:07:37 -0600

    Create ArraySwapper.java

---
+++ ArraySwapper.java
@@ -0,0 +1,27 @@
+class ArraySwapper {
+  
+  public int[] arraySwap(int []inputArray) {
+    
+    // Swap the two items in "inputArray" and return the output
+    int[] outputArray = new int[2];
+
+    // put your code here
+    
+    return outputArray;
+    
+  }
+  
+  public static void main(String[] args) {
+
+    ArraySwapper testSwapper = new ArraySwapper();
+    
+    int[] exampleInput = {1,2};
+    int[] exampleOutput = testSwapper.arraySwap(exampleInput);
+    
+    if (exampleInput[0] != exampleOutput[1] || exampleInput[1] != exampleOutput[0]) {
+      System.out.println("Swap failed");
+    } else {
+      System.out.println("Swap succeeded!");
+    }
+  }
+}
